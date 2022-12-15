99166166147449b58a4b6d4acbd3d70f2d595ddc
VvB-13 <vvb@rfid-im-blick.de>
2022-12-16 01:06:43 +0200

    Stage 1

--- cs50-final-project/app.py
+++ cs50-final-project/app.py
@@ -1,34 +1,85 @@
-from flask import Flask, render_template, redirect
-from cs50 import SQL
-from flask_session import Session
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, render_template, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, user_logged_in
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError, EqualTo
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "secret_key"
 
-db = SQL("sqlite:///operations_research.db")
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=3, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=3, max=20), EqualTo("password_confirm")], render_kw={"placeholder": "Password"})
+    password_confirm = PasswordField(validators=[InputRequired(), Length(
+        min=3, max=20)], render_kw={"placeholder": "Password (again)"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_username = User.query.filter_by(username=username.data).first()
+        if existing_username:
+            raise ValidationError("That username already exists. Please choose a different one.")
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=3, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=3, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login ")
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
 
-@app.route("/login")
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/login")
+
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
 
-@app.route("/tsp")
-def tsp():
-    return render_template("tsp.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect("/")
 
-@app.route("/knapsack")
-def knapsack():
-    return render_template("knapsack.html")
+    return render_template("login.html", form=form)
 
 @app.route("/logout")
+@login_required
 def logout():
-    return redirect("/")
+    logout_user()
+    return redirect("/login")
